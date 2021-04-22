4b6b9dfb2693e4ceaa421a23c1b163848b259818
ajengtresna99 <ajengtresna99@gmail.com>
2021-04-22 16:34:04 +0700

    https://github.com/ajengtresna99/UTS-FP.git

---
+++ database/migrations/2021_04_21_065332_create_surat_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuratTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surat', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('no_surat');
+            $table->String('tgl_surat');
+            $table->String('pengirim');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_surat');
+    }
+}
